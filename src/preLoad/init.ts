c892c4a2a44178dfdf7c8921a99bdd60461a21ac
bencascales <bencascales@gmail.com>
2025-02-11 18:04:47 -0300

    add sound to play button al choose symbol

--- src/preLoad/init.ts
+++ src/preLoad/init.ts
@@ -8,28 +8,29 @@ export default class init extends Phaser.Scene {
     this.load.image('Play', 'assets/Play.png');
     this.load.image('O', 'assets/O.png');
     this.load.image('X', 'assets/X.png');
+    this.load.audio('Tap', 'assets/Tap.mp3');
   }
   create() {
-    const playButton = this.add.image(400, 250, 'Play');
-    playButton.setInteractive();
+    const playButton = this.add.image(400, 250, 'Play').setInteractive();
     playButton.on('pointerdown', () => {
+      this.sound.add('Tap').play();
       playButton.destroy();
       this.chooseSymbol();
     });
   }
 
   chooseSymbol() {
-    const oButton = this.add.image(600, 250, 'O');
-    const xButton = this.add.image(200, 250, 'X');
-    oButton.setInteractive();
-    xButton.setInteractive();
+    const oButton = this.add.image(600, 250, 'O').setInteractive();
+    const xButton = this.add.image(200, 250, 'X').setInteractive();
     oButton.on('pointerdown', () => {
+      this.sound.add('Tap').play();
       oButton.destroy();
       xButton.destroy();
       this.scene.add('GameScene', new GameScene('O'));
       this.scene.start('GameScene');
     });
     xButton.on('pointerdown', () => {
+      this.sound.add('Tap').play();
       oButton.destroy();
       xButton.destroy();
       this.scene.add('GameScene', new GameScene('X'));
