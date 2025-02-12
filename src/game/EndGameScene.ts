88213bd64ec1cda6a987e980c1590a20feee2f57
bencascales <bencascales@gmail.com>
2025-02-11 21:59:52 -0300

    add particles

--- src/game/EndGameScene.ts
+++ src/game/EndGameScene.ts
@@ -17,6 +17,7 @@ export default class EndGameScene extends Phaser.Scene {
       message = '¡Draw!';
     } else {
       message = `¡Winner: ${this.gameEnd}!`;
+      this.WinninerParticles();
     }
     this.add
       .text(this.cameras.main.centerX, this.cameras.main.centerY - 200, message, {
@@ -38,5 +39,23 @@ export default class EndGameScene extends Phaser.Scene {
     this.scene.add('init', new init());
     this.scene.start('init');
     this.scene.remove();
+  }
+
+  WinninerParticles() {
+    const particles = this.add.particles(0, 0, 'Particle', {
+      x: { min: 0, max: this.cameras.main.width },
+      speedY: { min: 100, max: 300 }, // Velocidad vertical hacia abajo
+      speedX: { min: -50, max: 50 }, // Pequeño movimiento horizontal aleatorio
+      angle: 90, // Ángulo fijo hacia abajo
+      scale: { start: 0.5, end: 0, random: true }, // Escala aleatoria
+      rotate: { start: 0, end: 360, random: true }, // Rotación aleatoria
+      alpha: { start: 1, end: 0 }, // Fade out
+      gravityY: 50, // Gravedad adicional hacia abajo
+      lifespan: 3000, // Duración de vida de las partículas
+      blendMode: 'NORMAL', // Modo de mezcla
+      quantity: 10, // Cantidad de partículas emitidas por ciclo
+      frequency: 100, // Frecuencia de emisión
+    });
+    particles.flow(1000, 20);
   }
 }
